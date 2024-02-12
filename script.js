1414543bb3968f4ad6e2a29b7fc651c1c4736e25
Oliver Ardelean <148954618+Oliver-Ard@users.noreply.github.com>
2024-02-12 10:05:42 +0200

    Add UI manipulation for the basic operations

--- script.js
+++ script.js
@@ -1,43 +1,110 @@
-const displayOperation = document.querySelector(".display .operation");
-const displayResult = document.querySelector(".display .result");
+// ----HTML Elements----
+const operationDisplay = document.querySelector(".display .operation");
+const resultDisplay = document.querySelector(".display .result");
 const keypads = document.querySelector(".keypads");
+const equalOperator = document.querySelector(".equal");
 
-let operandOne;
-let operandTwo;
-let operator;
+keypads.addEventListener("click", clickKeys);
 
-function operate(symbol, num1, num2) {
+// ----Calculator Logic----
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+
+function operate(symbol, a, b) {
+	operationDisplay.textContent = `${firstOperand} ${operator} ${secondOperand} =`;
+
 	if (symbol === "+") {
-		return add(num1, num2);
-	} else if (symbol === "-") {
-		return subtract(num1, num2);
-	} else if (symbol === "*") {
-		return multiply(num1, num2);
-	} else if (symbol === "/") {
-		return divide(num1, num2);
+		return add(a, b);
+	} else if (symbol === "−") {
+		return subtract(a, b);
+	} else if (symbol === "×") {
+		return multiply(a, b);
+	} else if (symbol === "÷") {
+		return divide(a, b);
 	}
 }
 
-function add(num1, num2) {
-	return num1 + num2;
+function add(a, b) {
+	firstOperand = a + b;
+	resultDisplay.textContent = firstOperand;
 }
 
-function subtract(num1, num2) {
-	return num1 - num2;
+function subtract(a, b) {
+	firstOperand = a - b;
+	resultDisplay.textContent = firstOperand;
 }
 
-function multiply(num1, num2) {
-	return num1 * num2;
+function multiply(a, b) {
+	firstOperand = a * b;
+	resultDisplay.textContent = firstOperand;
 }
 
-function divide(num1, num2) {
-	return num1 / num2;
+function divide(a, b) {
+	firstOperand = a / b;
+	resultDisplay.textContent = firstOperand;
 }
 
-function checkKeyPads(e) {
-	if (e.target.className.includes("number")) {
-		console.log(e.target.textContent);
+// ----Manipulation of the UI
+function clickKeys(e) {
+	const targetElement = e.target;
+
+	if (targetElement.className.includes("number")) {
+		storeOperands(targetElement);
+	} else if (targetElement.className.includes("operator")) {
+		storeOperator(targetElement);
+	} else if (targetElement.className.includes("equal")) {
+		equalOperator.classList.add("finalResult");
+		operate(operator, +firstOperand, +secondOperand);
+	} else if (targetElement.textContent === "AC") {
+		clearInput();
 	}
 }
 
-keypads.addEventListener("click", checkKeyPads);
+function storeOperands(element) {
+	if (element.className.includes("number")) {
+		if (operator === "") {
+			storeFirstOperand(element);
+		} else {
+			storeSecondOperand(element);
+		}
+	}
+}
+
+function storeFirstOperand(element) {
+	firstOperand += element.textContent;
+	resultDisplay.textContent = firstOperand;
+}
+
+function storeSecondOperand(element) {
+	if (!equalOperator.className.includes("finalResult")) {
+		secondOperand += element.textContent;
+		resultDisplay.textContent = secondOperand;
+	} else {
+		clearInput();
+		storeFirstOperand(element);
+	}
+}
+
+function storeOperator(element) {
+	if (element.className.includes("operator")) {
+		if (equalOperator.className.includes("finalResult")) {
+			equalOperator.classList.remove("finalResult");
+			secondOperand = "";
+			operator = element.textContent;
+		} else {
+			operate(operator, +firstOperand, +secondOperand);
+			secondOperand = "";
+			operator = element.textContent;
+		}
+	}
+}
+
+function clearInput() {
+	operationDisplay.innerHTML = "&nbsp;";
+	resultDisplay.textContent = "0";
+	equalOperator.classList.remove("finalResult");
+	firstOperand = "";
+	secondOperand = "";
+	operator = "";
+}
